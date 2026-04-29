8e54b00f8d94c273f4df58a5e9a642c846e853b7
Loan <l.bernat@sileane.com>
2026-04-29 10:42:44 +0200

    updating color errors

--- src/magma_scenarios/scenarios/color_sorting/color_sorting_errors.py
+++ src/magma_scenarios/scenarios/color_sorting/color_sorting_errors.py
@@ -1,12 +1,14 @@
 import random
-from typing import Dict, Any, List, Optional
+from itertools import combinations
+from typing import Any, Dict, List, Optional
 
 from magma_core.base.data_structures import Observation
 from magma_core.base.data_structures.tools import ToolResult
 from magma_core.utils.env_utils import is_object_inside_target
-from magma_scenarios.templates.errors import MaskedObjectError, GraspFailureError
+from magma_scenarios.templates.errors import GraspFailureError, MaskedObjectError
 
 # On pourrait ajouter une erreur en mode boite inaccessible ou boite non détecté.
+
 
 def _get_scene_colors(obs: Observation) -> List[str]:
     colors = obs.add_constants.get("colors", obs.task_attributes.get("known_box_color", None))
@@ -17,110 +19,153 @@ def _get_scene_colors(obs: Observation) -> List[str]:
         )
     return list(colors)
 
-def _get_remaining_cubes(obs: Observation, env_id: int, colors: List[str]) -> List[str]:
-    remaining = []
+
+def _sample_error_objects(
+        obs: Observation,
+        env_id: int,
+        colors: List[str],
+        assignment: Dict[str, int],
+        max_nb: int,
+    ) -> List[str]:
+    remaining_by_color = {color: [] for color in colors}
+    sorted_counts = {color: 0 for color in colors}
+
+    if assignment == {}:
+        assignment = {color : 3 for color in colors}
+
+    out = []
+
     for obj_name, obj_pose in obs.maniskill_obs["extra"].items():
-        if "cube" not in obj_name:
-            continue
+        if "cube" in obj_name:
+            for color in colors:
+                if color in obj_name:
+                    if is_object_inside_target(obj_pose[env_id], obs.maniskill_obs["extra"][f"{color}_box_pose"][env_id]):
+                        sorted_counts[color] += 1
+                    else:
+                        remaining_by_color[color].append(obj_name)
+                    break
+    
+    needed_total = 0
+    slack_per_color : Dict[str,int] = {}
+    for c in colors:
+        need = max(assignment[c] - sorted_counts[c],0)
+        needed_total += need
+        if need > 0:
+            slack_per_color[c] = len(remaining_by_color[c]) - need
 
-        if all(
-            not is_object_inside_target(
-                obj_pose[env_id],
-                obs.maniskill_obs["extra"][f"{color}_box_pose"][env_id]
-            )
-            for color in colors
-        ):
-            remaining.append(obj_name)
-    return remaining
+    if needed_total <= 1:
+        color = None
+        for c in colors:
+            if assignment[c] - sorted_counts[c] == 1:
+                color = c
+                break
+        if color is None:
+            raise RuntimeError("Impossible fail")
+        
+        if len(remaining_by_color[color]) <= 1:
+            return []
+        nb = min(max_nb,len(remaining_by_color[color])-1)
+        return random.sample(remaining_by_color[color],k=nb)
+    
+    if len(slack_per_color) == 1: #case where only one color is interesting
+        for c in slack_per_color.keys():
+            n = len(remaining_by_color[c])
+            random.shuffle(remaining_by_color[c])
+            for obj in remaining_by_color[c]:
+                if max_nb <= 0 or n == 1:
+                    break
+                out.append(obj)
+                max_nb-=1
+                n-=1
+        
+    else:
+        # case where multiple color can be used
+        selected_color = random.choice(list(slack_per_color.keys()))
+        random.shuffle(remaining_by_color[selected_color])
+        out.extend(remaining_by_color[selected_color][:max_nb])
 
-def _build_detection_reason(context: Dict[str, Any], colors: List[str], remaining_objects: List[str]) -> str:
-    s = "This is the position of existing objects: "
-    for color in colors:
-        key = f"{color}_box"
-        if len(context[key]) == 0:
-            s += f"{color}_box is empty. "
-        else:
-            s += ",".join(context[key]) + f" are in the {color}_box. "
+    return out
 
-    if len(remaining_objects) > 0:
-        s += ",".join(remaining_objects) + " are not sorted."
-
-    return s
 
 class MaskRemainingCubesError(MaskedObjectError):
 
-    def __init__(self, max_masking = 2) -> None:
+    def __init__(
+            self,
+            max_masking: int = 2,
+            assignment: Optional[Dict[str, int]] = None,
+        ) -> None:
         super().__init__(tool_execution_target_key="target_name")
         self.max_nb = max_masking
+        self.assignment = {} if assignment is None else assignment.copy()
+
 
     def initialize(self, obs : Observation, env_id : int) -> Optional[Dict[str, Any]]:
         colors = _get_scene_colors(obs)
-        remaining = _get_remaining_cubes(obs, env_id, colors)
-
-        if len(remaining) <= 1:
-            return {
-                "masked" : [],
-                "colors": colors,
-            }
-        
-        if len(remaining) == 2:
-            nb = 1
-        else:
-            nb = random.randint(1,self.max_nb)
-        
-        masked = random.sample(remaining,k=nb)
-        return {
-            "masked" : masked,
-            "colors": colors,
+        d = {
+            "masked": _sample_error_objects(
+                obs,
+                env_id,
+                colors,
+                self.assignment,
+                self.max_nb,
+            ),
+            "colors": colors
         }
+        print(d)
+        return d
 
     def apply_post_verif(self, tool_result: ToolResult, arguments: Dict[str, Any]):
         if not tool_result.context:
             return
 
-        remaining_objects = list(tool_result.context.get("table",[]))
-        if len(remaining_objects) <= 1:
+        remaining_objects = list(tool_result.context.get("table", []))
+        masked = arguments.get("masked", [])
+        if len(remaining_objects) <= 1 or len(masked) == 0:
             return
 
-        masked = arguments.get("masked",[])
-        colors = arguments.get("colors", [])
+        for m in masked:
+            if m in remaining_objects:
+                remaining_objects.remove(m)
 
-        if len(masked) > 0:
-            for m in masked:
-                if m in remaining_objects:
-                    remaining_objects.remove(m)
+        colors = arguments.get("colors",[])
+        reason = "This is the position of existing objects: "
+        for color in colors:
+            key = f"{color}_box"
+            if len(tool_result.context[key]) == 0:
+                reason += f"{color}_box is empty. "
+            else:
+                reason += ",".join(tool_result.context[key]) + f" are in the {color}_box. "
 
-            tool_result.reason = _build_detection_reason(
-                tool_result.context,
-                colors,
-                remaining_objects,
-            )
+        if len(remaining_objects) > 0:
+            reason += ",".join(remaining_objects) + " are not sorted."
+
+        tool_result.reason = reason
+
 
 class GraspCubeFailureError(GraspFailureError):
 
     recovery_extra_steps = 1
 
-    def __init__(self, max_impossible = 1) -> None:
+    def __init__(
+            self,
+            max_impossible: int = 1,
+            assignment: Optional[Dict[str, int]] = None
+        ) -> None:
         super().__init__()
         self.max_nb = max_impossible
+        self.assignment = {} if assignment is None else assignment.copy()
 
     def initialize(self, obs : Observation, env_id : int) -> Dict[str, Any]:
         colors = _get_scene_colors(obs)
-        remaining = _get_remaining_cubes(obs, env_id, colors)
-
-        if len(remaining) <= 1:
-            return {
-                "inaccessible" : [],
-                "colors": colors,
-            }
         
-        if len(remaining) == 2:
-            nb = 1
-        else:
-            nb = random.randint(1,self.max_nb)
-        
-        impossible_to_grasp = random.sample(remaining,k=nb)
-        return {
-            "inaccessible" : impossible_to_grasp,
-            "colors": colors,
+        d = {
+            "inaccessible": _sample_error_objects(
+                obs,
+                env_id,
+                colors,
+                self.assignment,
+                self.max_nb,
+            )
         }
+        print(d)
+        return d
